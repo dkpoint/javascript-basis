15551a1e77a9cdda6e306012b3b5bc39a4bccc79
dkpoint <dkpoint.oh@gmail.com>
2022-08-28 22:35:31 +0900

    Feat: Use Nodemon

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+    # Use Nodemon
+        + Open Terminal : Ctrl + `
+        + Install Nodemon : npm install nodemon --location=global
+        + Run Js : nodemon index.js
+        + Exit Nodemon : Ctrl + C
+*/
+console.log('Hello World!');
