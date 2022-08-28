826d0cb8141137da28fa2e31f749016967e76199
dkpoint <dkpoint.oh@gmail.com>
2022-08-28 22:49:50 +0900

    Feat: 1-1 variable

---
+++ 01.variable/01.variable.js
@@ -0,0 +1,24 @@
+/*
+    @ 변수(Variable)
+        # 변수 선언(Variable declaration)
+            + let a;    (키워드 변수이름;)
+        
+        # 값의 재할당(Value reassigning)
+            + a = 1;    (변수이름 할당연산자 값;)
+            
+        # 변수 선언과 값의 할당(Variable declaration and assignment)
+            + let b = 0;    (키워드 변수이름 할당연산자 값;)
+        
+        # Reference
+            + Variables : developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Variables
+            + Keywords : developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#keywords
+*/
+
+let a;
+console.log(a);
+
+a = 1;
+console.log(a);
+
+let b = 0;
+console.log(b);
