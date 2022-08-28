0224017f15e6a26584112bf533802be2c110dc21
dkpoint <dkpoint.oh@gmail.com>
2022-08-28 22:51:28 +0900

    Feat: 1-2 naming

---
+++ 01.variable/02.naming.js
@@ -0,0 +1,24 @@
+/*
+    @ 변수 이름 지정(Naming Variables)
+        # 변수 규칙
+            + 라틴 문자(0-9, a-z, A-Z), _
+            + 대문자, 소문자를 구분한다.
+            + camelCase : likeThis
+            + 예약어를 사용할 수 없다.
+            + 숫자로 시작할 수 없다.
+            + 특수 문자를 사용할 수 없다.(예외: $, _)
+
+        # 변수 이름 지정
+            + 저장된 값의 의도를 나타낼 수 있는 이름으로 짓는다.
+        
+        # Reference
+            + Variable naming : developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Variables#an_aside_on_variable_naming_rules
+*/
+
+// Bad naming example
+let color1;
+let color2;
+
+// Good naming example
+let colorFont;
+let colorBackground;
