7ccbb07a469f04f19475a7d55746bf9a1c6de2ed
dkpoint <dkpoint@naver.com>
2022-09-01 00:53:23 +0900

    Feat: 2-1 expression

---
+++ 02.operator/01.expression.js
@@ -0,0 +1,22 @@
+/*
+    @ expression
+        # Reference
+            + JavaScript : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference
+            + Operators : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators
+*/
+
+// Statement
+let x;
+
+// expression
+let y = 1;
+
+// Template literal
+``;
+
+// Object initializer/literal syntax.
+{
+}
+
+// Array initializer/literal syntax.
+[];
