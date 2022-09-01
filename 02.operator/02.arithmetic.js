5847ebceeabe07d94367eb6b1c504a22a02c3521
dkpoint <dkpoint.oh@gmail.com>
2022-09-02 02:12:04 +0900

    Feat: 2-2 arithmetic

---
+++ 02.operator/02.arithmetic.js
@@ -0,0 +1,24 @@
+/*
+    @ Arithmetic operators
+        Arithmetic operators are the basic operators that we use to do sums in JavaScript
+
+        # Reference
+            + Arithmetic operators : https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Math#arithmetic_operators
+*/
+
+// +    Addition
+// -    Subtraction
+// *    Multiplication
+// /    Division
+// %    Remainder
+// **   Exponent
+
+console.log(50 + 50);
+console.log(101 - 1);
+console.log(50*2);
+console.log(201 % 101);
+console.log(Math.pow(10, 2));
+console.log(10 ** 2);
+
+console.log('50' + 50);
+console.log(typeof ('50'+50));
