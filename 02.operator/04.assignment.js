f2c39937ad6669bfd1dcf722fbebe885193b4571
dkpoint <dkpoint.oh@gmail.com>
2022-09-03 00:47:23 +0900

    Feat: 2-4 assignment

---
+++ 02.operator/04.assignment.js
@@ -0,0 +1,31 @@
+/*
+    @ Assignment operators
+        # Reference
+            + Addition assignment : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Addition_assignment
+            + Subtraction assignment : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Subtraction_assignment
+            + Multiplication assignment : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Multiplication_assignment
+            + Division assignment : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Division_assignment
+            + Remainder assignment : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder_assignment
+            + Exponentiation assignment : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Exponentiation_assignment
+*/
+
+let index = 1;
+console.log(index);
+
+index += 1; // index = index + 1;
+console.log(index);
+
+index -= 1; // index = index -1;
+console.log(index);
+
+index *= 33; // index = index * 33;
+console.log(index);
+
+index /= 3; // index = index / 3;
+console.log(index);
+
+index %= 7; // index = index % 7;
+console.log(index);
+
+index **= 2; // index = index ** 2;
+console.log(index);
