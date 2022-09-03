2746c5862da9aa4b0c5b3d9dae2f6617658f608e
dkpoint <dkpoint.oh@gmail.com>
2022-09-03 23:23:46 +0900

    Feat: 2-6 relational

---
+++ 02.operator/06.relational.js
@@ -0,0 +1,14 @@
+/*
+    @ Relational operators
+        # Reference
+            + Relational operators : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators#relational_operators
+*/
+
+console.log(2 > 1);
+console.log(1 > 2);
+
+console.log(1 < 2);
+console.log(2 < 1);
+
+console.log(2 >= 2);
+console.log(2 <= 2);
