2a4811a990addc1dfbee7d53fa8a9c1e6f7a3b02
dkpoint <dkpoint.oh@gmail.com>
2022-09-03 23:27:06 +0900

    Feat: 2-8 equality

---
+++ 02.operator/08.equality.js
@@ -0,0 +1,19 @@
+/*
+    @ Equality operators
+        # Reference
+            + Equality operators : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators#equality_operators
+*/
+
+console.log(1 == 1);
+console.log(2 == 1);
+
+console.log(2 != 1);
+console.log(1 != 1);
+
+console.log(2 == '2');
+
+console.log(2 === '2');
+
+console.log(1 != 1);
+
+console.log(1 !== '1');
