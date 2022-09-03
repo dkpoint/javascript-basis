08c763cdec55066c7600fc8e34689e8fbe5b984c
dkpoint <dkpoint.oh@gmail.com>
2022-09-04 00:23:41 +0900

    Feat: 3-2 ternary

---
+++ 03.control/02.ternary.js
@@ -0,0 +1,11 @@
+/*
+    @ Ternary operator
+        # Reference
+            + Ternary operator : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+*/
+
+let item = 'moon';
+item === 'star' ? console.log('⭐️') : console.log('??');
+
+let moon = item === 'moon' ? '🌙' : '??';
+console.log(moon);
