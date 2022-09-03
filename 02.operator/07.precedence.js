302cc1cb243ccbbd774f84ca50a02bc16c846974
dkpoint <dkpoint.oh@gmail.com>
2022-09-03 23:26:20 +0900

    Feat: 2-7 precedence

---
+++ 02.operator/07.precedence.js
@@ -0,0 +1,15 @@
+/*
+    @ Operator Precedence
+        # Reference
+            + Operator Precedence : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+*/
+
+console.log(1 + 2 * 3); // 1 + (2 * 3)
+
+console.log((1 + 2) * 3);
+
+console.log(2 * 3 ** 2); // 2 * (3 ** 2)
+
+console.log(typeof 1 + 2); // number2
+
+console.log(typeof (1 + 2)); // number
