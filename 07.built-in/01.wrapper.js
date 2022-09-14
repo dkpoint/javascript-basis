60d111dcbe8a81f1b6f19772d63c4587f6c44525
dkpoint <dkpoint.oh@gmail.com>
2022-09-14 23:41:11 +0900

    Feat: 7-1 wrapper

---
+++ 07.built-in/01.wrapper.js
@@ -0,0 +1,11 @@
+/*
+    @ Wrapper Object
+        # Reference
+            + Built-ins : https://developer.mozilla.org/en-US/docs/Web/JavaScript/reference#built-ins
+*/
+
+const number = 12; // Primitive Type : number
+console.log(number.toString()); // Wrapper Object : Number
+
+const text = 'Hello'; // Primitive Type : string
+console.log(text.length); // Wrapper Object : String
