d17f276354e0596603a6293f8986ee1442e7a692
dkpoint <dkpoint.oh@gmail.com>
2022-09-16 02:11:28 +0900

    Feat: 7-3 boolean

---
+++ 07.built-in/03.boolean.js
@@ -0,0 +1,21 @@
+/*
+    @ Boolean
+        # Reference
+            + Boolean : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean
+*/
+
+    // Falsy
+    console.log(`0:`, !!0);
+    console.log(`-0:`, Boolean(-0));
+    console.log(`null:`, !!null);
+    console.log(`NaN:`, Boolean(NaN));
+    console.log(`undefined:`, !!undefined);
+    console.log(`'':`, Boolean(''));
+
+    // Truthy
+    console.log(`1:`, Boolean(1));
+    console.log(`-1:`, !!-1);
+    console.log(`'0':`, Boolean('0'));
+    console.log(`'false':`, !!'false');
+    console.log(`[]:`, Boolean([]));
+    console.log(`{}:`, !!{});
