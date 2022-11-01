8a1a7870c3c6f2911b667a592b712075d5f55edb
dkpoint <dkpoint.oh@gmail.com>
2022-11-01 23:48:26 +0900

    Feat: 18-1 babel

---
+++ 18.babel/index.js
@@ -0,0 +1,13 @@
+/*
+    # Install Babel
+        + Open Terminal : Ctrl + `
+        + Initializer : npm init
+        + Install Nodemon : npm install --save-dev @babel/core @babel/cli @babel/preset-env
+        + Add a build script to package.json : "build": "babel index.js -w -o build/index.js"
+        + Run Build : npm run build
+        + You will see a new build folder.
+*/
+
+class Star {}
+const hello = () => {};
+const yes = Promise.resolve(1);
